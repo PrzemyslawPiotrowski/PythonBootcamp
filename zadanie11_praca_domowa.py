71d0aedc2dc3c8e1f85f05e097ff32844aa2ab6f
PrzemyslawPiotrowski <40918124+PrzemyslawPiotrowski@users.noreply.github.com>
2018-08-05 09:29:02 +0200

    Update zadanie11_praca_domowa.py

--- zadanie11_praca_domowa.py
+++ zadanie11_praca_domowa.py
@@ -1,21 +1,3 @@
-# Zadanie #11
-# Zaimplementuj klasy odpowiedzialne za tworzenie dokumenty w
-# składni MarkDown. Stwórz klase bazowa Element zawierajaca
-# podstawowa implementacje metody render() oraz kilka jej klas
-# pochodnych. Stwórz klase Document umozliwiajaca wyrendorowanie
-# dodanych elementów.
-# Przykład uzycia:
-# >>> document = Document()
-# >>> document.add_element(HeaderElement('Header'))
-# >>> document.add_element(LinkElement('ABC', 'abc.com'))
-# >>> document.add_element(Element('Simple'))
-# >>> document.render()
-# Header
-# ======
-# (ABC)[http://abc.com]
-# Simple
-
-
 class Element:
     def __init__(self, element):
         self.element = element
